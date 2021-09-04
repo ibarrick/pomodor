0ee96225db3a65b08b4b6f8f02eef56fce19e2d8
Ian Barrick <ian@servisuite.com>
2021-09-04 19:53:17 -0400

    ffs

--- src/layout/footer/Footer.js
+++ src/layout/footer/Footer.js
@@ -1,9 +1,5 @@
 import React from 'react'
 import styled from 'styled-components'
-import { useTheme } from '@material-ui/core'
-import FavoriteIcon from '@material-ui/icons/Favorite'
-import TwitterIcon from '@material-ui/icons/Twitter'
-import GitHubIcon from '@material-ui/icons/GitHub'
 
 export const Footer = () => {
 
