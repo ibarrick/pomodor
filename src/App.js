b68f78730968f7103e01287edef40d4801ad3c4d
Ian Barrick <ian@servisuite.com>
2021-09-04 19:09:49 -0400

    work

--- src/App.js
+++ src/App.js
@@ -3,14 +3,8 @@ import { BrowserRouter } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
 import { createGlobalStyle } from 'styled-components'
 import { CssBaseline } from '@material-ui/core'
-import { AppBar } from './layout/navigation/AppBar'
 import { MainContainer } from './layout/MainContainer'
-import { setUserInfo } from './data/auth/actions'
-import { startSetSettings } from './data/settings/actions'
 import { ThemeConfig } from './ThemeConfig'
-import { startSetLabels } from './data/labels/actions'
-import { startSetSessions } from './data/sessions/actions'
-import { setProgressVisibility } from './data/progress/actions'
 
 const App = () => {
   const dispatch = useDispatch()
